2ad4584ed38fef361657a499c8704761a8533c05
Rafael Moreno-Vozmediano <93595269+rmorvoz@users.noreply.github.com>
2021-11-03 13:35:27 +0100

    Update FaaSim-v1.0.py

--- FaaSim-v1.0.py
+++ FaaSim-v1.0.py
@@ -1,5 +1,30 @@
 # FaaSim Cloud/Edge Severless Platform Simulator V1.0
-
+#
+# Copyright 2021 Rafael Moreno-Vozmediano
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# IMPORTANT NOTE:
+# This simulator was created for academic and research purposes. 
+# This is an experimental version and its completeness and correctness are not guaranteed.
+# 
+# AUTHOR: 
+# Rafael Moreno-Vozmediano, PhD.
+# Dept. of Computer Architecture
+# Facultad de Informática (Computer Science School)
+# Universidad Complutese de Madrid. SPAIN
+#
 
 import random
 import time
